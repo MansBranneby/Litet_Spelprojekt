4222bbb4649e45a6a03883cfc3943cc6c16b5590
Clevinz <36393523+Clevinz@users.noreply.github.com>
2020-01-28 01:16:26 +0100

    Skrivit klart 2D OBB kollision.

--- BattleSphere/BattleSphere/OBB.cpp
+++ BattleSphere/BattleSphere/OBB.cpp
@@ -17,7 +17,38 @@ CollisionInfo OBB::intersectsWithOBB(BoundingVolume* other)
 	// SA stands for seperating axis
 	// SA: Ax
 	float projection = abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(vecToOBB, m_xAxis)));
-	//float proj = m_halfWD.x + abs(DirectX::XMVector2Dot(otherOBB->getHalfWD().y * otherOBB->getAxes()[0], m_xAxis));
+	float proj = m_halfWD.x
+		+ abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(DirectX::XMVectorScale(otherOBB->getAxes()[0], otherOBB->getHalfWD().x), m_xAxis)))
+		+ abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(DirectX::XMVectorScale(otherOBB->getAxes()[1], otherOBB->getHalfWD().y), m_xAxis)));
+	if (projection > proj)
+		return collisionInfo;
+		
+	// SA: Az
+	projection = abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(vecToOBB, m_zAxis)));
+	proj = m_halfWD.y
+		+ abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(DirectX::XMVectorScale(otherOBB->getAxes()[0], otherOBB->getHalfWD().x), m_zAxis)))
+		+ abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(DirectX::XMVectorScale(otherOBB->getAxes()[1], otherOBB->getHalfWD().y), m_zAxis)));
+	if (projection > proj)
+		return collisionInfo;
+
+	// SA: Bx
+	projection = abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(vecToOBB, otherOBB->getAxes()[0])));
+	proj = otherOBB->getHalfWD().x 
+		+ abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(DirectX::XMVectorScale(m_xAxis, m_halfWD.x), otherOBB->getAxes()[0])))
+		+ abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(DirectX::XMVectorScale(m_zAxis, m_halfWD.y), otherOBB->getAxes()[0])));
+	if(projection > proj)
+		return collisionInfo;
+
+	// SA: By
+	projection = abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(vecToOBB, otherOBB->getAxes()[1])));
+	proj = otherOBB->getHalfWD().y 
+		+ abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(DirectX::XMVectorScale(m_xAxis, m_halfWD.x), otherOBB->getAxes()[1])))
+		+ abs(DirectX::XMVectorGetX(DirectX::XMVector2Dot(DirectX::XMVectorScale(m_zAxis, m_halfWD.y), otherOBB->getAxes()[1])));
+	if (projection > proj)
+		return collisionInfo;
+
+	collisionInfo.m_colliding = true;
+
 	return collisionInfo;
 }
 
