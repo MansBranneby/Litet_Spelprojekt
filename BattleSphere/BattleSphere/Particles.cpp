d6fb1737005342a1452261eb3b699a95a9e6297d
Martin Persson <36561896+KaninKanon@users.noreply.github.com>
2020-02-26 16:02:28 +0100

    Revert "Revert "Started particle system""
    This reverts commit 9c444674934bb0999866820bc3619f7d368660da.

---
+++ BattleSphere/BattleSphere/Particles.cpp
@@ -0,0 +1,33 @@
+#include "Particles.h"
+
+Particles::Particles()
+{
+	m_computeShaderParticles = ComputeShader(L"ComputeShaderCulling.hlsl");
+	m_vertexShaderParticles = VertexShader(L"VertexShaderCulling.hlsl", 2);
+	m_geometryShaderParticles = GeometryShader(L"GeometryShaderCulling.hlsl");
+	m_pixelShaderParticles = PixelShader(L"PixelShaderCulling.hlsl");
+}
+
+Particles::~Particles()
+{
+	m_computeShaderParticles.release();
+	m_vertexShaderParticles.release();
+	m_geometryShaderParticles.release();
+	m_pixelShaderParticles.release();
+}
+
+void Particles::addParticles(XMVECTOR position, XMVECTOR color, XMVECTOR size, int amount, float velocity)
+{
+}
+
+void Particles::update(float dT)
+{
+}
+
+void Particles::draw()
+{
+	DX::getInstance()->getDeviceContext()->OMSetDepthStencilState(DX::getInstance()->getDSSEnabled(), 1);
+
+
+	DX::getInstance()->getDeviceContext()->OMSetDepthStencilState(DX::getInstance()->getDSSDisabled(), 1);
+}
